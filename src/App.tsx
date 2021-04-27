6d0aadccc7db52097fae7f5e863d95f057aa0932
0mkar <0mkar@protonmail.com>
2021-04-28 02:32:16 +0530

    Deploy compiled contract using ethcode.

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,14 @@
-import React, { useState } from 'react';
-import { createClient } from '@remixproject/plugin-webview';
+import React from 'react';
 import { EthcodePlugin } from './plugin/plugin';
 import './App.css';
 
-interface CompiledData {
-  target: string;
-  source: any;
-  version: string;
-  data: any;
-}
-// const pluginOpts: Partial<PluginOptions<Readonly<IRemixApi>>> = {}
+const client = new EthcodePlugin();
 
 function App() {
-  const [compiledData, setCompiled] = useState<CompiledData>();
-  const client = new EthcodePlugin();
-  client.onload(() => {
-    console.log('Ethcode Remix Plugin loaded....');
-    // client.on('solidity', 'compilationFinished', (target: any, source: any, version: any, data: any) => {
-    //   console.log("compile finished", target, source, version,  data);
-    //   setCompiled({
-    //     target,
-    //     source,
-    //     version,
-    //     data
-    //   });
-    // })
-  })
   const handleDeploy = () => {
-    if(compiledData && Object.keys(compiledData).length > 0) {
-      console.log("Will deploy....");
-      console.log(compiledData.data);
-      // Call ethcode extension to load deploy data
-      // client.call('solidity', 'compile', compiledData.data);
-      // client.call('extension-api', 'deploy');
+    console.log("Will deploy if compiled....");
+    if(client.compiled && Object.keys(client.compiled).length > 0) {
+      client.exec();
     }
   }
   return (
--- src/plugin/plugin.ts
+++ src/plugin/plugin.ts
@@ -2,16 +2,37 @@ import { PluginClient } from '@remixproject/plugin';
 import { createClient } from '@remixproject/plugin-webview';
 
 export class EthcodePlugin extends PluginClient {
-  callBackEnabled: boolean = false;
+  callBackEnabled: boolean = true;
+  compiled: any = {};
   constructor() {
     super();
-    this.name = 'ethcode';
-    this.methods = ["deploy", "dismiss"];
-    // @ts-ignore
     createClient(this);
+    this.methods = ["exec"];
     this.onload()
-      .then(() => {
-        console.log('Plugin loaded....');
+      .then(async () => {
+        console.log('Ethcode plugin loaded....');
+        await this.setCallBacks();
       })
+      .catch(async (e) => {
+        console.log("ERROR CONNECTING", e);
+      });
+  }
+  async setCallBacks() {
+    this.on('solidity', 'compilationFinished', (target: any, source: any, version: any, data: any) => {
+      console.log("Success! compilation finished.");
+      this.compiled = {
+        target,
+        source,
+        version,
+        data
+      };
+    })
+  }
+  async exec() {
+    try {
+      this.call('vscodeExtAPI', 'exec', 'ethential.ethcode', 'loadCompiled', [this.compiled]);
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
